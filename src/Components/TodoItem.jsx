70cfb362e9d3b93a7efc0b1e84f1f725cce646ad
alimardon28 <doniyorovalimardon369@gmail.com>
2022-02-28 09:49:56 +0500

    finished todolist

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -2,9 +2,11 @@ function TodoItem({ todo, handleCheck, onDelete }) {
 
     return (
         <div className="todolist__item">
-            <input className="todolist__item__check" onChange={(e)=> {handleCheck(e, todo.id)}} checked={todo?.isCompleted} type="checkbox" />
             <p className="todolist__item__title">{ todo?.title}</p>
-            <button onClick={()=>{onDelete(todo.id)}} className="todolist__item__btn">del</button>
+            <div className="InputBox">
+               <input className="todolist__item__check" onChange={(e)=> {handleCheck(e, todo.id)}} checked={todo?.isCompleted} type="checkbox" />
+              <button onClick={()=>{onDelete(todo.id)}} className="todolist__item__btn btn btn-danger"><i className="bi bi-x-lg"></i></button>
+            </div>
         </div>)
 }
 
--- src/Components/TodosList.jsx
+++ src/Components/TodosList.jsx
@@ -5,8 +5,6 @@ import { getAll, setItem } from "../utils/localStoroge"
 function TodoList() {
     const [todos, setTodos] = useState([])
     const handleCheck = (e, id) => {
-        console.log(id)
-        console.log(e)
             setTodos(state => {
                 const newTodos =  state.map(item => {
                     if (item.id === id) {
@@ -64,18 +62,18 @@ function TodoList() {
     }
     return <div className="todolist">
         <form onSubmit={addTodo} className="todolist__input">
-            <input className="todolist__input__field"  type="text" />
-            <button>add</button>
+            <input className="todolist__input__field" placeholder="Todo ..." type="text" />
+            <button className="todoBtn"><i className="bi bi-plus-lg"></i></button>
         </form>
         {
-            todos.length === 0 ? "Todos not found" : todos.map(todo => {
+            todos.length === 0 ? <p className="tosdoss">Todos not found</p> : todos.map(todo => {
                 return <TodoItem key={todo.id} todo={todo} onDelete={handleDelete} handleCheck={ handleCheck}/>
             })
         }
         <div className="todolist__filters">
-            <button onClick={handleShowAll}>All</button>
-            <button onClick={handleUnCompleted}>UnCompleted</button>
-            <button onClick={handleCompleted}>Completed</button>
+            <button className="AllBtn btn bg-primary text-light" onClick={handleShowAll}>All</button>
+            <button className="unCompleted btn bg-success text-light" onClick={handleUnCompleted}>UnCompleted</button>
+            <button className="completed btn bg-warning text-light" onClick={handleCompleted}>Completed</button>
         </div>
     </div>
 }
